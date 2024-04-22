1d7461d481678a1dffb63022960fa319f6ccd0a8
awesomebunny2 <awesomebunny@bellsouth.net>
2024-04-22 11:52:43 -0400

    bug fix for group update

--- Home.js
+++ Home.js
@@ -3399,7 +3399,10 @@ async function onTableChanged(eventArgs) {
 
                 //console.log(hyperlink.hyperlink);
 
-                if (hyperlink.hyperlink || formula.startsWith("=HYPERLINK")) {
+                //* For some reason formula.startsWith stopped working at some point and started breaking some sheets
+                //* Took it out for now but maybe will need to revisit later
+                //if (hyperlink.hyperlink || formula.startsWith("=HYPERLINK")) {
+                if (hyperlink.hyperlink) {
                     leCell.clear(Excel.ClearApplyTo.hyperlinks);
                     // leCell.format.font.underline = false;
                     // leCell.format.font.color = "black";
@@ -3427,7 +3430,11 @@ async function onTableChanged(eventArgs) {
 
                 //console.log(hyperlink.hyperlink);
 
-                if (hyperlink.hyperlink || formula.startsWith("=HYPERLINK")) {
+                //* For some reason formula.startsWith stopped working at some point and started breaking some sheets
+                //* Took it out for now but maybe will need to revisit later
+                //if (hyperlink.hyperlink || formula.startsWith("=HYPERLINK")) {
+                if (hyperlink.hyperlink) {
+
                     leCell.clear(Excel.ClearApplyTo.hyperlinks);
                     // leCell.format.font.underline = false;
                     // leCell.format.font.color = "black";
